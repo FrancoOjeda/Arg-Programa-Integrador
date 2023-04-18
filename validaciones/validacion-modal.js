044833bf94273c0838b87e2b2cc37c9bf191e259
Nahuel Monteagudo <nahuel_sebastian_lucker@hotmail.com>
2023-04-18 16:12:42 -0300

    validacion modal

--- validaciones/validacion-modal.js
+++ validaciones/validacion-modal.js
@@ -1,65 +1,68 @@
-$(document).ready(function () {
-    $("#form-modal").submit(function(event){
-        event.preventDefault(); // Prevenir comportamiento predeterminado del formulario
-    let nombre = $("#nombreM").val().trim();
-    // Seleccionamos el input nombre, el metodo val() retorna el valor del input, el metodo trim() corta los espacios en blanco al principio y al final del string
+$(document).ready(function() {
+$("#form-modal").submit(function(event){
+    event.preventDefault();
 
-    let apellido = $("#apellidoM").val().trim(); 
-    //let telefonoM = $("#telefonoM").val().trim();  //si agrego esta variable, deja de funcionar todo el codigo del js
-    let email = $("#emailM").val().trim();
-    let mensaje = $("#mensajeM").val().trim();
+    let nombreModal = $("#nombreModal").val().trim();
+    let apellidoModal = $("#apellidoModal").val().trim();
+    let telefonoModal = $("#telefonoModal").val().trim();
+    let emailModal = $("#emailModal").val().trim();
+    let IDmodal = $("#IDmodal").val().trim();
+    let mensajeModal = $("#mensajeModal").val().trim(); 
 
-    // Realizamos lo mismo para cada campo
-
-    // Validacion nombre
-    if(nombre ===""){
-        $("#nombreM").addClass("campo-invalido");
-    // Si el campo nombre esta vacio, se agrega una clase nueva al input
+    if(nombreModal === ""){
+        $("#nombreModal").addClass("campo-invalido");
         $("#mensaje-error-nombreM").removeClass("d-none")
-    // Tambien se remueve la clase d-none del parrafo de mensaje de error
-    } else {
-        $("#nombreM").removeClass("campo-invalido");
+    }
+    else{
+        $("#nombreModal").removeClass("campo-invalido");
         $("#mensaje-error-nombreM").addClass("d-none");
-        // Si el input se envia correctamente, se remueven las clases que no son necesarias, y se agrega la clase d-none al parrafo de error
     };
 
-    // Validacion apellido
-    if(apellido ===""){
-        $("#apellidoM").addClass("campo-invalido");
-        $("#mensaje-error-apellidoM").removeClass("d-none");
-    } else {
-        $("#apellidoM").removeClass("campo-invalido");
+    if(apellidoModal === ""){
+        $("#apellidoModal").addClass("campo-invalido");
+        $("##mensaje-error-apellidoM").removeClass("d-none");
+    }
+    else{
+        $("#apellidoModal").removeClass("campo-invalido");
         $("#mensaje-error-apellidoM").addClass("d-none");
     };
 
-    /*if(telefonoM ===""){
-        $("#telefonoM").addClass("campo-invalido");
+    if(telefonoModal ===""){
+        $("#telModal").addClass("campo-invalido");
         $("#mensaje-error-telefonoM").removeClass("d-none");
-    } else {
-        $("#telefonoM").removeClass("campo-invalido");
+    }
+    else{
+        $("#telModal").removeClass("campo-invalido");
         $("#mensaje-error-telefonoM").addClass("d-none");
-    }; */
+    };
 
-    // Validacion email
-    if(email ===""){
-        $("#emailM").addClass("campo-invalido");
-        $("#mensaje-error-emailM").removeClass("d-none");
-    } else {
-        $("#emailM").removeClass("campo-invalido");
+    if(emailModal === ""){
+        $("#emailModal").addClass("campo-invalido");
+        $("#mensaje-error-emailM").removeClass("d-none")
+    }
+    else{
+        $("#emailModal").removeClass("campo-invalido");
         $("#mensaje-error-emailM").addClass("d-none");
     };
 
-    // Validacion mensaje
+    if(IDmodal === ""){
+        $("#IDmodal").addClass("campo-invalido");
+        $("#mensaje-error-nombreM").removeClass("d-none")
+    }
+    else{
+        $("#IDmodal").removeClass("campo-invalido");
+        $("#mensaje-error-nombreM").addClass("d-none");
+    };
 
-    if(mensaje ===""){
-        $("#mensajeM").addClass("campo-invalido");
-        $("#mensaje-error-mensajeM").removeClass("d-none");
-    } else {
-        $("#mensajeM").removeClass("campo-invalido");
+    if(mensajeModal === ""){
+        $("#mensajeModal").addClass("campo-invalido");
+        $("#mensaje-error-ID").removeClass("d-none")
+    }
+    else{
+        $("#mensajeModal").removeClass("campo-invalido");
         $("#mensaje-error-mensajeM").addClass("d-none");
     };
 
+})
 
-
-    })
 })
